d0cc2b7a00b1ccc49f296fbe8f126626091c3dd8
Yoki Jati Perkasa <yokijati@gmail.com>
2025-03-01 23:09:04 +0700

    Add New Function Get All Content & Delete Content by Id FIX

--- src/controllers/contentController.js
+++ src/controllers/contentController.js
@@ -1,5 +1,7 @@
 import { Hono } from "hono";
 import addContent from "../services/contentService.js";
+import getAllContents from "../services/contentService.js";
+import deleteContentById from "../services/contentService.js";
 
 const contentController = new Hono();
 
@@ -19,7 +21,7 @@ contentController.post("/add", async (c) => {
 });
 
 // Route untuk mendapatkan semua konten
-contentController.get('/all', async (c) => {
+contentController.get("/all", async (c) => {
   try {
     const contents = await getAllContents();
     return c.json({ success: true, data: contents }, 200);
@@ -30,11 +32,14 @@ contentController.get('/all', async (c) => {
 });
 
 // Route untuk menghapus konten berdasarkan ID
-contentController.delete('/delete/:id', async (c) => {
+contentController.delete("/delete/:id", async (c) => {
   try {
     const { id } = c.req.param(); // Ambil ID dari parameter URL
     await deleteContentById(id);
-    return c.json({ success: true, message: 'Content deleted successfully' }, 200);
+    return c.json(
+      { success: true, message: "Content deleted successfully" },
+      200
+    );
   } catch (error) {
     console.error(error);
     return c.json({ success: false, message: error.message }, 400);
